47358aca9318f0d5878659f835a8b69f4e29555c
Ilya Sinegubkin <ilya.sinegubkin@gmail.com>
2022-06-26 23:27:38 +0300

    feat(Photos): add image preview in photos

--- src/Photo/index.jsx
+++ src/Photo/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { v4 as uuid4 } from "uuid";
 import { ErrorView, Spinner } from "@components";
 import { useLoadData } from "@hooks/useLoadData";
+import ImageViewer from "react-simple-image-viewer";
 
 const photoContainerStyle = {
   display: "flex",
@@ -22,36 +23,64 @@ const descriptionStyle = {
 
 export const Photo = () => {
   const [loadingStatus, photos, reload] = useLoadData("/photos/");
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+  const openImageViewer = (index) => () => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  };
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
 
   return (
-    <Container style={{ marginTop: 72 }}>
-      <Row>
-        <Col style={{ marginBottom: 72 }}>
-          <h1>ФОТО</h1>
-        </Col>
-      </Row>
-      {loadingStatus === "rejected" && <ErrorView reload={reload} />}
-      {loadingStatus === "pending" && (
-        <Spinner animation="grow" variant="light" />
-      )}
-      {loadingStatus === "resolved" && (
+    <>
+      <Container style={{ marginTop: 72 }}>
         <Row>
-          <Col style={photoContainerStyle}>
-            {photos &&
-              photos.map(({ image, description }) => (
-                <div style={photoCardStyle} key={uuid4()}>
-                  <Image
-                    src={image}
-                    style={{ marginBottom: 16 }}
-                    width={356}
-                    height={238}
-                  />
-                  <p style={descriptionStyle}>{description}</p>
-                </div>
-              ))}
+          <Col style={{ marginBottom: 72 }}>
+            <h1>ФОТО</h1>
           </Col>
         </Row>
+        {loadingStatus === "rejected" && <ErrorView reload={reload} />}
+        {loadingStatus === "pending" && (
+          <Spinner animation="grow" variant="light" />
+        )}
+        {loadingStatus === "resolved" && (
+          <Row>
+            <Col style={photoContainerStyle}>
+              {photos &&
+                photos.map(({ image, description }, index) => (
+                  <div style={photoCardStyle} key={uuid4()}>
+                    <Image
+                      src={image}
+                      style={{ marginBottom: 16 }}
+                      width={356}
+                      height={238}
+                      onClick={openImageViewer(index)}
+                    />
+                    <p style={descriptionStyle}>{description}</p>
+                  </div>
+                ))}
+            </Col>
+          </Row>
+        )}
+      </Container>
+
+      {isViewerOpen && (
+        <ImageViewer
+          src={photos.map((data) => data.image)}
+          currentIndex={currentImage}
+          onClose={closeImageViewer}
+          disableScroll
+          backgroundStyle={{
+            backgroundColor: "rgba(0, 0, 0, 0.9)",
+          }}
+          closeOnClickOutside={true}
+        />
       )}
-    </Container>
+    </>
   );
 };
